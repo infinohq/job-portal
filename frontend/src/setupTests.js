a1344ecb0cd03c000bb1cba38dde6368568ed238
Vinay Kakade <vinaykakade@gmail.com>
2024-09-18 10:13:23 +0530

    Add log messages to /Users/vinaykakade/ws/infinohq/job-portal

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { trace, context } from '@opentelemetry/api';
+
+const tracer = trace.getTracer('default');
+
+const span = tracer.startSpan('import-jest-dom');
+context.with(trace.setSpan(context.active(), span), () => {
+  span.addEvent('jest-dom imported');
+  console.log('Span for jest-dom import ended');
+  span.end();
+  console.log('Span ended for import-jest-dom');
+});
